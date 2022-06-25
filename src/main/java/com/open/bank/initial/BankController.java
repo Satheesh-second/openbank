d3487044656cad9d96dc9118922a6c29fd4c8a7d
Satheesh-second <108170900+Satheesh-second@users.noreply.github.com>
2022-06-25 11:18:25 +0300

    Update BankController.java (#2)
    Co-authored-by: satheesh-ch <cherukumallisatheesh@gmail.com>

--- src/main/java/com/open/bank/initial/BankController.java
+++ src/main/java/com/open/bank/initial/BankController.java
@@ -10,7 +10,7 @@ public class BankController {
 
     public ResponseEntity generateCustomerBase()
     {
-        //Added the revised commnet by another user with branch1
+        //Commented added by satheesh-second with the branch1
         return null;
     }
 }
